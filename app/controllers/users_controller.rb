71404c74a3af1432ec8c97f836f258016c4bc7dd
lmonty22 <lindsay.montgomery@olympian.org>
2020-04-13 18:07:15 -0400

    fixed new user issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
+        session[:user_id] = @user.id
         redirect_to user_path(@user)
     end
 
