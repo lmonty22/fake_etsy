9ada46f7088958ce4456c3ebb7583421a8326e20
bret-gibson <bret.gibson@gmail.com>
2020-04-15 11:58:26 -0400

    Fixed seeding for images

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,11 +26,15 @@ Order.destroy_all
 OrderItem.destroy_all
 Review.destroy_all
 
+
 user1 = User.create(username: "lmonty22", first_name: "Lindsay", last_name: "Montgomery", password: "password")
 user2 = User.create(username: "bret-gib", first_name: "Bret", last_name: "Gibson", password: "password")
-user1.image.attach("shop.png")
-user2.image.attach("50393443.jpg")
+user1.image.attach(io: File.open('app/assets/images/shop.png'), filename: 'shop.png')
+user2.image.attach(io: File.open('app/assets/images/50393443.jpg'), filename: '50393443.jpg')
+
 shop1 = Shop.create(user_id: user1.id, name: "Lindsay's shop ", description: "Things I could never make. ")
+shop1.image.attach(io: File.open('app/assets/images/shop.png'), filename: 'shop.png')
+
 item1 = Item.create(name: "Face Mask", shop_id: shop1.id, price: 10.00, quantity: 30, description: "Protects against COVID-19 spread.")
 item2 = Item.create(name: "Dog bandana", shop_id: shop1.id, price: 25.00, quantity: 10, description: "Makes your dog look cool.")
 order1 = Order.create(user_id: user2.id )
